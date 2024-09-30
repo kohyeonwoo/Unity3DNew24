77a5da7fd599bc587ecd83b1b130f63d762237d5
kohyeonwoo <49010769+kohyeonwoo@users.noreply.github.com>
2024-09-30 23:27:53 +0900

    240929-240930
    골렘 애니메이션 중 공격 중지 위한 함수 추가

--- 3DNew/Assets/Scripts/Boss/Golem/Golem.cs
+++ 3DNew/Assets/Scripts/Boss/Golem/Golem.cs
@@ -2,7 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Golem : MonoBehaviour
+public class Golem : MonoBehaviour, IDamageable
 {
-   
+
+    private Animator animator;
+
+
+    public void EndAttack1()
+    {
+        animator.SetBool("attack1", false);
+    }
+
+    public void EndAttack2()
+    {
+        animator.SetBool("attack2", false);
+    }
+
+    public void Damage(float Damage)
+    {
+        
+    }
+
 }
